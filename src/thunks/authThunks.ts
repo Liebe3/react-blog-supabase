95532d9123b2076bd8e0324eead6d82436c7b9c2
dyaaah <95646179+Liebe3@users.noreply.github.com>
2026-01-14 09:38:37 +0800

    feature: auth
    store user profile in Supabase during registration

--- src/thunks/authThunks.ts
+++ src/thunks/authThunks.ts
@@ -1,69 +1,94 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { supabase } from "../services/supabase";
 
-import type { LoginCredentials, RegisterCredentials, User } from "../services/types/auth.types";
+import type {
+  LoginCredentials,
+  RegisterCredentials,
+  User,
+} from "../services/types/auth.types";
 
 // Register Thunk
 export const registerThunk = createAsyncThunk<
-	User,
-	RegisterCredentials,
-	{ rejectValue: string }> ("auth/register",
-		async ({email, password, confirmPassword}, {rejectWithValue}) => {
+  User,
+  RegisterCredentials,
+  { rejectValue: string }
+>(
+  "auth/register",
+  async (
+    { email, password, confirmPassword, profile },
+    { rejectWithValue }
+  ) => {
+    if (password !== confirmPassword) {
+      return rejectWithValue("Passwords do not match");
+    }
 
-			if(password !== confirmPassword){
-				return rejectWithValue("Passwords do not match")
-			}
-			
-			const {data, error } = await supabase.auth.signUp({email, password})
+    const { data, error } = await supabase.auth.signUp({ email, password });
 
-			if(error){
-				return rejectWithValue(error.message)
-			}
+    if (error) {
+      console.error("Supabase signup error:", error);
+      return rejectWithValue(error.message);
+    }
 
-			if(!data.user || !data.user.email){
-				return rejectWithValue("Registration failed")
-			}
+    if (!data.user || !data.user.email) {
+      return rejectWithValue("Registration failed");
+    }
 
-			return{
-				id: data.user.id,
-				email: data.user.email!
-			}
+    const { data: profileData, error: profileError } = await supabase
+      .from("profiles")
+      .insert({
+        id: data.user.id,
+        first_name: profile.first_name,
+        last_name: profile.last_name,
+      })
+      .select();
 
-		}
-	);
+    if (profileError) {
+      return rejectWithValue(profileError.message);
+    }
 
-	// Login Thunk
+    return {
+      id: data.user.id,
+      email: data.user.email!,
+      profile: profileData[0],
+    };
+  }
+);
+
+// Login Thunk
 export const loginThunk = createAsyncThunk<
-	User,
-	LoginCredentials,
-	{	 rejectValue: string }> ("auth/login",
-		async ({email, password}, {rejectWithValue}) => {
-			const {data, error } = await supabase.auth.signInWithPassword({email, password})
+  User,
+  LoginCredentials,
+  { rejectValue: string }
+>("auth/login", async ({ email, password }, { rejectWithValue }) => {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
 
-			if(error){
-				return rejectWithValue(error.message)
-			}
+  if (error) {
+    return rejectWithValue(error.message);
+  }
 
-			if(!data.user || !data.user.email){
-				return rejectWithValue("No user found")
-			}
+  if (!data.user || !data.user.email) {
+    return rejectWithValue("No user found");
+  }
 
-			// "!" non null operator
-			return{
-				id: data.user.id,
-				email: data.user.email!
-			}
-		}
-)
+  // "!" non null operator
+  return {
+    id: data.user.id,
+    email: data.user.email!,
+  };
+});
 
 // Logout Thunk
-export const logoutThunk = createAsyncThunk<void, void, {rejectValue: string}>("auth/logout",
-	async (_, {rejectWithValue}) => {
+export const logoutThunk = createAsyncThunk<
+  void,
+  void,
+  { rejectValue: string }
+>("auth/logout", async (_, { rejectWithValue }) => {
+  const { error } = await supabase.auth.signOut();
 
-		const {error} = await supabase.auth.signOut()
-
-		if(error){
-			return rejectWithValue(error.message)
-		}
-	}
-)
+  if (error) {
+    return rejectWithValue(error.message);
+  }
+});
