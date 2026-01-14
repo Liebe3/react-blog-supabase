fccaab0633b19283adb6f1a4e49cee8b005b28cb
dyaaah <95646179+Liebe3@users.noreply.github.com>
2026-01-14 09:35:59 +0800

    chore: supabase
    fix Supabase env typo

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,6 +1,7 @@
+// src/services/supabase.ts
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonkey = import.meta.env.Vite_SUPABASE_ANON_KEY;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonkey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
