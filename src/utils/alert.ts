7769d1bdc9d0b0906b78a63b3e8a175d9ee0559c
dyaaah <95646179+Liebe3@users.noreply.github.com>
2026-01-16 05:35:45 +0800

    feature: alert
    add confirm delete and formdate

--- src/utils/alert.ts
+++ src/utils/alert.ts
@@ -1,6 +1,10 @@
+// utils/alert.ts
 import Swal from "sweetalert2";
+import type { AppDispatch } from "../store";
 
-//  Success notification
+/**
+ * Success notification
+ */
 export const ShowSucess = (message: string) => {
   Swal.fire({
     icon: "success",
@@ -8,10 +12,47 @@ export const ShowSucess = (message: string) => {
   });
 };
 
-//  Error notification
+/**
+ * Error notification
+ */
 export const ShowError = (message: string) => {
   Swal.fire({
     icon: "error",
     text: message,
   });
 };
+
+export const ConfirmDelete = async (
+  dispatch: AppDispatch,
+  thunk: (id: string) => any,
+  id: string,
+  successMessage = "Item deleted successfully"
+) => {
+  const result = await Swal.fire({
+    title: "Are you sure?",
+    text: "This action cannot be undone.",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#10b981",
+    cancelButtonColor: "#ef4444",
+    confirmButtonText: "Yes, delete it!",
+  });
+
+  if (result.isConfirmed) {
+    try {
+      await dispatch(thunk(id)).unwrap();
+      ShowSucess(successMessage);
+    } catch (error) {
+      ShowError((error as string) || "Failed to delete");
+    }
+  }
+};
+
+  export const formatDate = (dateString: string) => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    });
+  };
