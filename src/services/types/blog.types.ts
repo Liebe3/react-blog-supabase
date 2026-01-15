9f0afc7043875c274e50103b993863445f73788b
dyaaah <95646179+Liebe3@users.noreply.github.com>
2026-01-16 05:50:46 +0800

    feature: blogs
    add author info, pagination, and search to Blog and BlogState

--- src/services/types/blog.types.ts
+++ src/services/types/blog.types.ts
@@ -1,25 +1,34 @@
 export interface Blog {
-    id: string;
-		title: string;
-		content: string;
-		author_id: string;
-		created_at: string;
-		updated_at: string;
+  id: string;
+  title: string;
+  content: string;
+  author_id: string;
+  created_at: string;
+  updated_at: string;
+  author?: {
+    first_name: string;
+    last_name: string;
+  };
 }
 
 export interface BlogState {
-	blogs: Blog[];
-	loading: boolean;
-	error: string | null;
+  blogs: Blog[];
+  userBlogs: Blog[];
+  loading: boolean;
+  error: string | null;
+  currentPage: number;
+  pageSize: number;
+  totalBlogs: number;
+  searchTerm: string;
 }
 
-export interface CreateBlogInput{
-	title: string;
-	content: string;
+export interface CreateBlogInput {
+  title: string;
+  content: string;
 }
 
-export interface UpdateBlogInput{
-	id: string
-	title: string;
-	content: string
+export interface UpdateBlogInput {
+  id: string;
+  title: string;
+  content: string;
 }
