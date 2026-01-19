008d7fb6cd6dc02d53e727b4cd130d1781c970c1
dyaaah <95646179+Liebe3@users.noreply.github.com>
2026-01-19 13:54:29 +0800

    feature: comment
    add comment slice to redux store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import blogReducer from "./slices/blogSlice";
+import commentReducer from "./slices/commentSlice";
 
 export const store = configureStore({
-    reducer: {
-      auth: authReducer,
-			blog: blogReducer
-    }
-})
+  reducer: {
+    auth: authReducer,
+    blog: blogReducer,
+    comment: commentReducer,
+  },
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
